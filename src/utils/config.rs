bf9ba58c67196acdd065ec6e60a53fe6797c53a9
Jonathan Kingsley <me@jfkingsley.co.uk>
2016-12-17 14:14:42 +0000

    Fixed build issues

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,4 +1,4 @@
-fn loadConfig(path: &str) {
+fn load_config(path: &str) {
     // let value = toml::Parser::new(toml).parse().unwrap();
     // println!("{:?}", value);
     println!("Loading config from: {}\n", path);
